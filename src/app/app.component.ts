269b6dfaf78d0843aecfd839dd459061a02b3421
Dobó Gergely <dgergely98@windowslive.com>
2020-03-25 20:05:10 +0100

    Implement AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,45 @@
 import { Component } from '@angular/core';
+import { ProductService } from './services/product/product.service';
+import { take } from 'rxjs/operators';
+import { ProductWithID } from './models/product';
+import { isNil } from 'lodash';
+import { MatDialog } from '@angular/material';
+import { FormComponent } from './components/form/form.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+	constructor(private readonly dialog: MatDialog, public readonly productService: ProductService) {}
+
+	public updateProduct(product: ProductWithID): void {
+		this.dialog
+			.open(FormComponent, { data: product })
+			.afterClosed()
+			.pipe(take(1))
+			.subscribe((updatedProduct) => {
+				if (!isNil(updatedProduct) && !isNil(updatedProduct.data)) {
+					this.productService.updateProduct(updatedProduct.data);
+				}
+			});
+	}
+
+	public createProduct(): void {
+		this.dialog
+			.open(NewProductComponent)
+			.afterClosed()
+			.pipe(take(1))
+			.subscribe((product) => {
+				if (!isNil(product) && !isNil(product.data)) {
+					this.productService.createProduct(product.data);
+				}
+			});
+	}
+
+	public deleteProduct(product: ProductWithID): void {
+		this.productService.deleteProduct(product);
+	}
+}
