279597039ea6f51b35de87e82eb9ff99fd410d35
Dobó Gergely <dgergely98@windowslive.com>
2020-03-25 19:41:59 +0100

    Implement ProductService

---
+++ src/app/services/product/product.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Product, ProductWithID } from 'src/app/models/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ProductService {
+	private productCollection: AngularFirestoreCollection<Product>;
+	private _products$: Observable<ProductWithID[]>;
+
+	constructor(private readonly firebase: AngularFirestore) {
+		this.productCollection = this.firebase.collection<Product>('product');
+		this._products$ = this.productCollection.snapshotChanges().pipe(
+			map((actions) =>
+				actions.map((action) => {
+					const data = action.payload.doc.data() as Product;
+					const id = action.payload.doc.id;
+					return { id, ...data } as ProductWithID;
+				})
+			)
+		);
+	}
+
+	public get products$(): Observable<ProductWithID[]> {
+		return this._products$;
+	}
+
+	public createProduct(product: Product): void {
+		this.productCollection.add(product);
+	}
+
+	public deleteProduct(product: ProductWithID): void {
+		this.firebase.doc('product/' + product.id).delete();
+	}
+
+	public updateProduct(product: ProductWithID): void {
+		this.firebase.doc('product/' + product.id).update(product);
+	}
+}
