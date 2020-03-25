c07e4654968b8af7b04309b88ce1fe7244fbb2fd
Dobó Gergely <dgergely98@windowslive.com>
2020-03-25 20:03:25 +0100

    Implement NewProductComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { ProductsComponent } from './components/products/products.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-	declarations: [AppComponent, ProductsComponent, HeaderComponent],
+	declarations: [AppComponent, ProductsComponent, HeaderComponent, NewProductComponent],
 	imports: [
 		BrowserModule,
 		AngularFireModule.initializeApp(environment.firebaseConfig),
 		AngularFirestoreModule.enablePersistence(),
 		BrowserAnimationsModule,
-		MaterialModule
+		MaterialModule,
+		FormsModule
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
+	entryComponents: [NewProductComponent]
 })
 export class AppModule {}
