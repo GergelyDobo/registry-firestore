1a27855db31c10dee5c1e6c531194214f010f8e6
Dobó Gergely <dgergely98@windowslive.com>
2020-03-25 19:51:32 +0100

    Install @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule.enablePersistence()],
+	imports: [
+		BrowserModule,
+		AngularFireModule.initializeApp(environment.firebaseConfig),
+		AngularFirestoreModule.enablePersistence(),
+		BrowserAnimationsModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
