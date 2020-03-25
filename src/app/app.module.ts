773637e4ef90bd7a96cc57330acbf44a7b69b8ef
Dobó Gergely <dgergely98@windowslive.com>
2020-03-25 19:35:35 +0100

    Configure AngularFireModule and AngularFirestoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule.enablePersistence()],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {}
